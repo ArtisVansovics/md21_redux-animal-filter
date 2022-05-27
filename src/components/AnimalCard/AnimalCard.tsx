34d0401812a5e39f02cfcc561abf3828d07706cf
Artis Vansovics <vansovics.artis@gmail.com>
2022-05-27 22:19:53 +0300

    Defined AnimalCard look and composition

--- src/components/AnimalCard/AnimalCard.tsx
+++ src/components/AnimalCard/AnimalCard.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './AnimalCard.module.scss';
 
-const AnimalCard = () => {
-  const a = 0;
+type AnimalProps = {
+  name: string;
+  species: string;
+  imgUrl: string;
+}
 
-  return (
-    <div>
-      hhh
-    </div>
-  );
-};
+const AnimalCard = ({ name, species, imgUrl }: AnimalProps) => (
+  <div className={styles.card}>
+    <img className={styles.image} src={imgUrl} alt={name} />
+    <h3 className={styles.title}>{name}</h3>
+    <p className={styles.species}>{`Species: ${species}`}</p>
+  </div>
+);
 
 export default AnimalCard;
