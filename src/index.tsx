615bb31b5dda4ca21423cf8010ec4d9d1214c41f
Artis Vansovics <vansovics.artis@gmail.com>
2022-05-29 18:13:58 +0300

    Updated store.ts and index.tsx files in order to run Redux Persist properly

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
-import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './store/store';
 import reportWebVitals from './reportWebVitals';
-import store from './store/store';
+import './index.css';
+import App from './App';
 import './components/styles/cssReset.scss';
 import './components/styles/flexboxgrid.scss';
 import './components/styles/base.scss';
+import Loader from './components/Loader/Loader';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,50 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import animalReducer from './slices/animalSlice';
 
-const store = configureStore({
-  reducer: {
-    animalList: animalReducer,
-  },
+const rootReducer = combineReducers({
+  animalList: animalReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
 export default store;
+
+// const store = configureStore({
+//   reducer: {
+//     animalList: animalReducer,
+//   },
+// });
